2d0e08ff7590b1a36c8a53b32e2ff6022fc32e61
Eswara Sai <eswarasai.v@gmail.com>
2021-09-28 19:20:36 +0530

    Updated build

--- build/module/abi/index.d.ts
+++ build/module/abi/index.d.ts
@@ -995,6 +995,55 @@ export declare const Abi: {
         name?: undefined;
         outputs?: undefined;
     })[];
+    xAssetCLR: ({
+        anonymous: boolean;
+        inputs: {
+            indexed: boolean;
+            internalType: string;
+            name: string;
+            type: string;
+        }[];
+        name: string;
+        type: string;
+        outputs?: undefined;
+        stateMutability?: undefined;
+    } | {
+        inputs: {
+            internalType: string;
+            name: string;
+            type: string;
+        }[];
+        name: string;
+        outputs: {
+            internalType: string;
+            name: string;
+            type: string;
+        }[];
+        stateMutability: string;
+        type: string;
+        anonymous?: undefined;
+    } | {
+        inputs: ({
+            internalType: string;
+            name: string;
+            type: string;
+            components?: undefined;
+        } | {
+            components: {
+                internalType: string;
+                name: string;
+                type: string;
+            }[];
+            internalType: string;
+            name: string;
+            type: string;
+        })[];
+        name: string;
+        outputs: never[];
+        stateMutability: string;
+        type: string;
+        anonymous?: undefined;
+    })[];
     xBNT: ({
         anonymous: boolean;
         inputs: {
