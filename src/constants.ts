66aa48a7195627ef81b38a954b063718786d0157
Nora Liu <noraliu0830@gmail.com>
2021-12-01 15:12:06 +0800

    feat: add nft abis

--- src/constants.ts
+++ src/constants.ts
@@ -129,3 +129,14 @@ export const BORROW = 'borrow'
 export const REPAY = 'repay'
 export const SUPPLY = 'supply'
 export const WITHDRAW = 'withdraw'
+
+// NFT
+export const ARBITRUM_NFT_CORE = 'ArbitrumNFTCore'
+export const L2_NFT = 'L2NFT'
+
+export const GM = 'GM'
+export const GA = 'GA'
+export const GN = 'GN'
+export const WAGMI = 'WAGMI'
+
+export const LIQUIDITY_POOL = 'liquidityPool'
